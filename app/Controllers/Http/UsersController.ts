e07e90b77d7e3696c245b48241d9c2c5d7269a42
David Sotto Mayor <ddsmmf@gmail.com>
2022-10-18 18:21:30 -0400

    Creating the business logic to create a new user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,23 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class UsersController {
-  public async createUser({ response }: HttpContextContract) {
-    return response.created()
+  public async createUser({ request, response }: HttpContextContract) {
+    const userData = request.only(['username', 'password', 'email', 'avatar'])
+
+    const existingEmail = await User.findBy('email', userData.email)
+    if (existingEmail) {
+      return response.conflict({ message: 'Email already in use. Please pick a different one.' })
+    }
+
+    const existingUsername = await User.findBy('username', userData.username)
+    if (existingUsername) {
+      return response.conflict({ message: 'Username already in use. Please pick a different one.' })
+    }
+
+    const newUser = await User.create(userData)
+    const savedUser = await newUser.save()
+
+    return response.created({ message: 'User created successfully!', user: savedUser })
   }
 }
